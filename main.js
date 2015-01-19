3a1c3397a19b4fa86a0652fe64f543d180bb54e5
Ryan budny <rb1econ@gmail.com>
2015-01-19 12:14:57 -0700

    done

--- main.js
+++ main.js
@@ -1,12 +1,59 @@
-// 1 
-for (var i=0; i<=500; i+=100) {
-	console.log(i);
-};
+// // 1 
+// for (var i=0; i<=500; i+=100) {
+// 	console.log(i);
+// };
 
-// 2
-for (var i=1; i<=64; i*=2) {
-	console.log(i);
-};
+// // 2
+// for (var i=1; i<=64; i*=2) {
+// 	console.log(i);
+// };
 
-// 3
-for (var i=1; i<=3; i)
+// // 3
+// for (var i=1; i<=3; i++) {
+// 	console.log(i);
+// 	console.log(i);
+// 	console.log(i);
+// };
+
+// // 4 
+// for (var i=0; i<=10; i+=2) {
+// 	console.log(i);
+// };
+
+// // 5
+// for (var i=3; i<=15; i+=3) {
+// 	console.log(i);	
+// }
+
+// // 6
+// for (var i=9; i>=0; i--) {
+// 	console.log(i);
+// }
+
+// 7
+// for (var i=0; i<=3; i++) {
+// 	console.log(i);
+// 	for (var i=0; i<=3; i++) {
+// 	console.log(i);
+// 		for (var i=0; i<=3; i++) {
+// 		console.log(i);
+// 			for (var i=0; i<=3; i++) {
+// 			console.log(i);
+	
+// 			}
+	
+// 		}
+// 	}
+// }
+
+for (var i=0; i<=3; i++) {
+	for (var x=0;x<=3;x++) {
+	console.log(x);
+	x++;
+	console.log(x);
+	x++;
+	console.log(x);
+	x++;
+	console.log(x);
+}
+}
